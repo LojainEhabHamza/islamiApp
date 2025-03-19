df79be4ff5f0472af79ed65b9f398bbae5401cd2
LojainHamza <lojainehab17@gmail.com>
2025-03-19 15:03:03 +0200

    modified radio tab logic.

--- lib/features/home/tabs/RadioTab/presentation/manager/radio_provider.dart
+++ lib/features/home/tabs/RadioTab/presentation/manager/radio_provider.dart
@@ -7,6 +7,11 @@ class RadioProvider with ChangeNotifier {
   String? currentPlayingUrl;
   Map<String, bool> isPlayingMap = {};
   Map<String, bool> isVolumeUpMap = {};
+  Map<String, double> previousVolumeMap = {};
+
+  RadioProvider() {
+    audioPlayer.setVolume(1.0);
+  }
 
   Future<void> play(String url) async {
     if (currentPlayingUrl == url && isPlaying) {
@@ -20,6 +25,12 @@ class RadioProvider with ChangeNotifier {
       currentPlayingUrl = url;
       isPlaying = true;
       isPlayingMap[url] = true;
+
+      if (!isVolumeUpMap.containsKey(url)) {
+        isVolumeUpMap[url] = true;
+        previousVolumeMap[url] = 1.0;
+        await audioPlayer.setVolume(1.0);
+      }
     }
     notifyListeners();
   }
@@ -33,13 +44,23 @@ class RadioProvider with ChangeNotifier {
 
   Future<void> setVolume(String url, bool isVolumeUp) async {
     isVolumeUpMap[url] = isVolumeUp;
+
     if (currentPlayingUrl == url) {
-      await audioPlayer.setVolume(isVolumeUp ? 2.0 : 0.0);
+      if (isVolumeUp) {
+        // عند unmute، نعيد الصوت إلى القيمة السابقة
+        double restoredVolume = previousVolumeMap[url] ?? 1.0;
+        await audioPlayer.setVolume(restoredVolume);
+      } else {
+        // عند mute، نحفظ القيمة الحالية للصوت ونضبطه على 0.0
+        previousVolumeMap[url] = audioPlayer.volume; // حفظ قيمة الصوت الحالية
+        await audioPlayer.setVolume(0.0);
+      }
     }
+
     notifyListeners();
   }
 
   bool getVolumeStatus(String url) {
-    return isVolumeUpMap[url] ?? true;
+    return isVolumeUpMap[url] ?? true; // القيمة الافتراضية هي true (الصوت يعمل)
   }
 }
