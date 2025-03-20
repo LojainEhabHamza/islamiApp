76bcdaeda17e02800e977232a8ce8e8e70608794
LojainHamza <lojainehab17@gmail.com>
2025-03-20 04:56:48 +0200

    prayer model done.

--- lib/core/api/api_manager/api_manager.dart
+++ lib/core/api/api_manager/api_manager.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:islami/features/home/tabs/RadioTab/data/models/RadioResponseModel.dart';
 import 'package:islami/features/home/tabs/RadioTab/data/models/RecitersResponseModel.dart';
+import 'package:islami/features/home/tabs/TimeTab/data/models/PrayerResponseModel.dart';
 
 class ApiManager{
   static Future<RadioResponseModel>getRadioData()async{
@@ -21,6 +23,18 @@ class ApiManager{
       var response = await http.get(uri);
       var jsonResponse = jsonDecode(response.body);
       return RecitersResponseModel.fromJson(jsonResponse);
+    }catch(e){
+      throw e;
+    }
+  }
+
+  static Future<PrayerResponseModel>getPrayerDate()async{
+    try{
+      var date = DateFormat('dd-MM-YYYY').format(DateTime.now());
+      Uri uri = Uri.parse('https://api.aladhan.com/v1/timingsByCity/$date?city=alex&country=egypt');
+      var response = await http.get(uri);
+      var jsonResponse = jsonDecode(response.body);
+      return PrayerResponseModel.fromJson(jsonResponse);
     }catch(e){
       throw e;
     }
